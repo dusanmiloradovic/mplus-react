e0b1d2e4e80f34bd9492c596457cd4f5ffea690d
Dusan Miloradovic <dusan@gasco.ae>
2019-01-08 17:12:57 +0400

    eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
+import { eslint } from "rollup-plugin-eslint";
 
 export default {
   input: "src/mplus-react.js",
@@ -18,6 +19,20 @@ export default {
     }),
     commonjs({
       include: "node_modules/**"
+    }),
+    eslint({
+      envs: ["browser", "mocha"],
+      useEslintrc: false,
+      parser: "babel-eslint",
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+          modules: true,
+          experimentalObjectRestSpread: true
+        }
+      }
     }),
     babel({
       exclude: ["node_modules/**"]
