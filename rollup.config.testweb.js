7ec338eedfa9e51f2f66f66e209c3bb64c543f47
Dusan Miloradovic <dusan.m@maximoplus.com>
2020-06-12 17:25:42 +0400

    more offline

--- rollup.config.testweb.js
+++ rollup.config.testweb.js
@@ -2,6 +2,7 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import replace from "rollup-plugin-replace";
+import { string } from "rollup-plugin-string";
 
 export default {
   input: "src/test-app.js",
@@ -24,6 +25,7 @@ export default {
     }),
     babel({
       exclude: ["node_modules/**"]
-    })
+    }),
+    string({ include: "**/*.sql" })
   ]
 };
